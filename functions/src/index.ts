97967c2152a1ca97a430b38888b5654275b5e547
shehanp12 <shehanp068@gmail.com>
2020-12-04 15:56:25 +0530

    install dependecies

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,18 @@
 import * as functions from 'firebase-functions';
+const express = require('express');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+
+const app = express();
+import * as cors from 'cors';
+app.use(cors());
+
+
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+
+const boardingHouse = require("./controllers/boardingController");
+app.use('/boardingHouse', boardingHouse);
+exports.api = functions.https.onRequest(app);
